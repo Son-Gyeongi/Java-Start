941c304ec95ac2ca88e5d05ae123f94c7270f1bb
Son-Gyeongi <thsruddl77@gmail.com>
2025-07-27 22:41:32 +0900

    11. 변수 선언과 초기화

---
+++ src/variable/Var4.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var4 {
+
+    public static void main(String[] args) {
+        int a; // 변수 선언
+        int b; // a,b 메모리 공간 2개 차지
+
+        int c, d; // 연달아 변수 선언
+    }
+}
---
+++ src/variable/Var6.java
@@ -0,0 +1,10 @@
+package variable;
+
+public class Var6 {
+
+    public static void main(String[] args) {
+        // 꼭 변수는 초기화 해야 한다.
+        int a; // 컴파일 단계에서 최적화를 한다.
+        // System.out.println(a); // 주석을 풀면 컴파일 오류 발생
+    }
+}
