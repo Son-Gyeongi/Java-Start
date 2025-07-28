203108a2ca1d1006f48c63a07b39a5d5a6bb3ef0
Son-Gyeongi <thsruddl77@gmail.com>
2025-07-28 21:33:19 +0900

    section4. 연산자 문제와 풀이

---
+++ src/operator/ex/OperationEx3.java
@@ -0,0 +1,10 @@
+package operator.ex;
+
+// 합격 범위
+public class OperationEx3 {
+
+    public static void main(String[] args) {
+        int score = 80;
+        System.out.println(80 <= score && score <= 100);
+    }
+}
